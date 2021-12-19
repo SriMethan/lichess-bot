448a70661c4bf8e01e0457d58c95ac84a2991bf7
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2021-12-19 10:03:01 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -233,8 +233,8 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
         def __init__(self, room):
             self.room = room
     opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
-    conversation.send_reply(SendLine('player'), f'Good Luck @{opponent}')
-    conversation.send_reply(SendLine('spectator'), f'Good Luck @{opponent}')
+    conversation.send_reply(SendLine('player'), f'All the best  {opponent}')
+    conversation.send_reply(SendLine('spectator'), f'Welcome to my game spectators!')
     
     logger.info("+++ {}".format(game))
 
@@ -307,8 +307,8 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
         except StopIteration:
             break
     
-    conversation.send_reply(SendLine('player'), f'Good Game')
-    conversation.send_reply(SendLine('spectator'), f'Good Game')
+    conversation.send_reply(SendLine('player'), f'Good game,Thank you !')
+    conversation.send_reply(SendLine('spectator'), f'Thanks for watch my game !')
 
     engine.stop()
     engine.quit()
