d8d99a765d3cbba2f3858d8033982f9a545d49e1
Methan Sritharan <methanroyal@gmail.com>
2022-02-11 04:42:08 +0000

    Minor Changes

--- config.py
+++ config.py
@@ -46,6 +46,7 @@ def load_config(config_file):
         if working_dir and not os.path.isdir(working_dir):
             raise Exception(f"Your engine's working directory `{working_dir}` is not a directory.")
              
+    variant=game.perf_name         
 
     if variant=="standard":    
         engine = os.path.join(CONFIG["engine"]["dir"], CONFIG["engine"]["name"])
