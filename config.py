61a74cdfe78d3f2e87b3fada375b3f8c26555b50
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2022-02-12 11:20:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ def load_config(config_file):
         if working_dir and not os.path.isdir(working_dir):
             raise Exception(f"Your engine's working directory `{working_dir}` is not a directory.")
             
-    engine = os.path.join(CONFIG["engine"]["dir"], CONFIG["engine"]["name"])
+        engine = os.path.join(CONFIG["engine"]["dir"], CONFIG["engine"]["name"])
 
         if not os.path.isfile(engine) and CONFIG["engine"]["protocol"] != "homemade":
             raise Exception("The engine %s file does not exist." % engine)
