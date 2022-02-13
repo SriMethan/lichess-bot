a88958b5ec437c4c400134f5db55925e45c0f1d2
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2022-02-13 14:54:05 +0530

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -3,45 +3,51 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class Conversation:
-    def __init__(self, game, engine, xhr, version, challenge_queue):
-        self.game = game
-        self.engine = engine
-        self.xhr = xhr
-        self.version = version
-        self.challengers = challenge_queue
+lass Chatter:
+    def __init__(self, config: dict) -> None:
+        self.cpu = self._get_cpu()
+        self.ram_message = self._get_ram()
+        self.draw_message = self._get_draw_message(config)
 
-    command_prefix = "!"
-
-    def react(self, line, game):
-        logger.info("*** {} [{}] {}: {}".format(self.game.url(), line.room, line.username, line.text.encode("utf-8")))
-        if (line.text[0] == self.command_prefix):
-            self.command(line, game, line.text[1:].lower())
-
-    def command(self, line, game, cmd):
-        if cmd == "commands" or cmd == "help":
-            self.send_reply(line, "Supported commands: !wait (wait a minute for my first move), !name, !howto, !eval, !queue")
-        elif cmd == "wait" and game.is_abortable():
-            game.ping(60, 120, 120)
-            self.send_reply(line, "Waiting 60 seconds...")
-             elif cmd == "name":
-            name = game.me.name
-            self.send_reply(line, "{} running {} (lichess-bot v{})".format(name, self.engine.name(), self.version))
-        elif cmd == "howto":
-            self.send_reply(line, "How to run: Check out 'Lichess Bot API'")
-        elif cmd == "eval" and line.room == "spectator":
-            stats = self.engine.get_stats(for_chat=True)
-            self.send_reply(line, ", ".join(stats))
-        elif cmd == "eval":
-            self.send_reply(line, "I don't tell that to my opponent, sorry.")
-        elif cmd == "queue":
-            if self.challengers:
-                challengers = ", ".join(["@" + challenger.challenger_name for challenger in reversed(self.challengers)])
-                self.send_reply(line, "Challenge queue: {}".format(challengers))
-            else:
-                self.send_reply(line, "No challenges queued.")
+    def react(self, command: str, lichess_game: Lichess_Game) -> str:
+        if command == 'cpu':
+            return self.cpu
         elif command == 'draw':
             return self.draw_message
+        elif command == 'engine':
+            return lichess_game.engine.id["name"]
+        elif command == 'eval':
+            return lichess_game.last_message
+        elif command == 'name':
+            return f'{lichess_game.username} running {lichess_game.engine.id["name"]} (Torom\'s BotLi)'
+        elif command == 'ram':
+            return self.ram_message
+        else:
+            return 'Supported commands: !cpu, !draw, !engine, !eval, !name, !ram'
+
+    def _get_cpu(self) -> str:
+        if os.path.exists('/proc/cpuinfo'):
+            with open('/proc/cpuinfo', 'r') as cpuinfo:
+                while line := cpuinfo.readline():
+                    if line.startswith('model name'):
+                        cpu = line.split(': ')[1]
+                        cpu = cpu.replace('(R)', '')
+                        cpu = cpu.replace('(TM)', '')
+                        return cpu
+
+        if cpu := platform.processor():
+            return cpu
+
+        return 'Unknown'
+
+    def _get_ram(self) -> str:
+        mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
+        mem_gib = mem_bytes/(1024.**3)
+
+        return f'{mem_gib:.1f} GiB'
+
+    def _get_draw_message(self, config: dict) -> str:
+        draw_enabled = config['engine']['offer_draw']['enabled']
 
         if not draw_enabled:
             return 'This bot will neither accept nor offer draws.'
@@ -52,19 +58,10 @@ class Conversation:
 
         return f'The bot offers draw at move {min_game_length} or later ' \
             f'if the eval is within +{max_score:.2f} to -{max_score:.2f} for the last {consecutive_moves} moves.'
-    
-  
-
-    def send_reply(self, line, reply):
-        self.xhr.chat(self.game.id, line.room, reply)
-
-    def send_message(self, room, message):
-        if message:
-            self.send_reply(ChatLine({"room": room}), message)
 
 
-class ChatLine:
-    def __init__(self, json):
-        self.room = json.get("room")
-        self.username = json.get("username")
-        self.text = json.get("text")
+class Chat_Message:
+    def __init__(self, chatLine_event: dict) -> None:
+        self.username: str = chatLine_event['username']
+        self.text: str = chatLine_event['text']
+        self.room: str = chatLine_event['room']
