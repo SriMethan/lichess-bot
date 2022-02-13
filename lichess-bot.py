9188704d39e6a5daf81bbc0da10a10b4647b0d91
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2022-02-13 11:44:04 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -600,6 +600,20 @@ def setup_board(game):
 
     return board
 
+def _get_move_overhead(self) -> int:
+        move_overhead = self.initial_time // 60 - self.increment
+
+        return move_overhead if move_overhead >= 0 else 0
+
+     def _has_time(self, min_time: int) -> bool:
+        return self.white_time >= min_time if self.is_white else self.black_time >= min_time
+
+     def _reduce_own_time(self, milliseconds: int) -> None:
+        if self.is_white:
+            self.white_time -= milliseconds
+        else:
+            self.black_time -= milliseconds
+
 
 def is_engine_move(game, board):
     return game.is_white == (board.turn == chess.WHITE)
