a150cd395a0a158839a32e522931f6d32585fd50
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2022-02-18 11:35:29 +0530

    Update engine_wrapper.py

--- engine_wrapper.py
+++ engine_wrapper.py
@@ -1,244 +1,247 @@
    
 import os
-import chess.engine
+import chess
+from chess import engine
 import backoff
 import subprocess
-import logging
-from enum import Enum
-
-logger = logging.getLogger(__name__)
-
 
 @backoff.on_exception(backoff.expo, BaseException, max_time=120)
-def create_engine(config):
+def create_engine(config, board):
     cfg = config["engine"]
     engine_path = os.path.join(cfg["dir"], cfg["name"])
     engine_type = cfg.get("protocol")
     engine_options = cfg.get("engine_options")
-    draw_or_resign = cfg.get("draw_or_resign") or {}
     commands = [engine_path]
     if engine_options:
         for k, v in engine_options.items():
             commands.append("--{}={}".format(k, v))
 
-    stderr = None if cfg.get("silence_stderr", False) else subprocess.DEVNULL
+    silence_stderr = cfg.get("silence_stderr", False)
 
     if engine_type == "xboard":
-        Engine = XBoardEngine
-    elif engine_type == "uci":
-        Engine = UCIEngine
-    elif engine_type == "homemade":
-        Engine = getHomemadeEngine(cfg["name"])
-    else:
-        raise ValueError(
-            f"    Invalid engine type: {engine_type}. Expected xboard, uci, or homemade.")
-    options = remove_managed_options(cfg.get(engine_type + "_options", {}) or {})
-    return Engine(commands, options, stderr, draw_or_resign)
+        return XBoardEngine(board, commands, cfg.get("xboard_options", {}) or {}, silence_stderr)
 
-
-def remove_managed_options(config):
-    def is_managed(key):
-        return chess.engine.Option(key, None, None, None, None, None).is_managed()
-
-    return {name: value for (name, value) in config.items() if not is_managed(name)}
-
-
-class Termination(str, Enum):
-    MATE = 'mate'
-    TIMEOUT = 'outoftime'
-    RESIGN = 'resign'
-    ABORT = 'aborted'
-    DRAW = 'draw'
-
-
-class GameEnding(str, Enum):
-    WHITE_WINS = '1-0'
-    BLACK_WINS = '0-1'
-    DRAW = '1/2-1/2'
-    INCOMPLETE = '*'
-
-
-PONDERPV_CHARACTERS = 12  # the length of ', ponderpv: '
-MAX_CHAT_MESSAGE_LEN = 140  # maximum characters in a chat message
+    return UCIEngine(board, commands, cfg.get("uci_options", {}) or {}, silence_stderr)
 
 
 class EngineWrapper:
-    def __init__(self, commands, options, stderr, draw_or_resign):
-        self.scores = []
-        self.draw_or_resign = draw_or_resign
-        self.go_commands = options.pop("go_commands", {}) or {}
-        self.last_move_info = {}
 
-    def search_for(self, board, movetime, ponder, draw_offered):
-        return self.search(board, chess.engine.Limit(time=movetime // 1000), ponder, draw_offered)
+    def __init__(self, board, commands, options=None, silence_stderr=False):
+        pass
 
-    def first_search(self, board, movetime, draw_offered):
-        # No pondering after the first move since a different clock is used afterwards.
-        return self.search(board, chess.engine.Limit(time=movetime // 1000), False, draw_offered)
+    def set_time_control(self, game):
+        pass
 
-    def search_with_ponder(self, board, wtime, btime, winc, binc, ponder, draw_offered):
-        cmds = self.go_commands
-        movetime = cmds.get("movetime")
-        if movetime is not None:
-            movetime = float(movetime) / 1000
-        time_limit = chess.engine.Limit(white_clock=wtime / 1000,
-                                        black_clock=btime / 1000,
-                                        white_inc=winc / 1000,
-                                        black_inc=binc / 1000,
-                                        depth=cmds.get("depth"),
-                                        nodes=cmds.get("nodes"),
-                                        time=movetime)
-        return self.search(board, time_limit, ponder, draw_offered)
+    def first_search(self, board, movetime):
+        pass
 
-    def offer_draw_or_resign(self, result, board):
-        if self.draw_or_resign.get('offer_draw_enabled', False) and len(self.scores) >= self.draw_or_resign.get('offer_draw_moves', 5):
-            scores = self.scores[-self.draw_or_resign.get('offer_draw_moves', 5):]
-            pieces_on_board = len([board.piece_type_at(sq) for sq in chess.SQUARES if board.piece_type_at(sq)])
-            scores_near_draw = lambda score: abs(score.relative.score(mate_score=40000)) <= self.draw_or_resign.get('offer_draw_score', 0)
-            if len(scores) == len(list(filter(scores_near_draw, scores))) and pieces_on_board <= self.draw_or_resign.get('offer_draw_pieces', 10):
-                result.draw_offered = True
-
-        if self.draw_or_resign.get('resign_enabled', False) and len(self.scores) >= self.draw_or_resign.get('resign_moves', 3):
-            scores = self.scores[-self.draw_or_resign.get('resign_moves', 3):]
-            scores_near_loss = lambda score: score.relative.score(mate_score=40000) <= self.draw_or_resign.get('resign_score', -1000)
-            if len(scores) == len(list(filter(scores_near_loss, scores))):
-                result.resigned = True
-        return result
-
-    def search(self, board, time_limit, ponder, draw_offered):
-        result = self.engine.play(board, time_limit, info=chess.engine.INFO_ALL, ponder=ponder, draw_offered=draw_offered)
-        self.last_move_info = result.info.copy()
-        self.scores.append(self.last_move_info.get("score", chess.engine.PovScore(chess.engine.Mate(1), board.turn)))
-        result = self.offer_draw_or_resign(result, board)
-        self.last_move_info["ponderpv"] = board.variation_san(self.last_move_info.get("pv", []))
-        self.print_stats()
-        return result
+    def search(self, board, wtime, btime, winc, binc):
+        pass
 
     def print_stats(self):
-        for line in self.get_stats():
-            logger.info(f"{line}")
-
-    def get_stats(self, for_chat=False):
-        info = self.last_move_info.copy()
-        stats = ["depth", "nps", "nodes", "score", "ponderpv"]
-        if for_chat:
-            bot_stats = [f"{stat}: {info[stat]}" for stat in stats if stat in info and stat != "ponderpv"]
-            len_bot_stats = len(", ".join(bot_stats)) + PONDERPV_CHARACTERS
-            ponder_pv = info["ponderpv"]
-            ponder_pv = ponder_pv.split()
-            try:
-                while len(" ".join(ponder_pv)) + len_bot_stats > MAX_CHAT_MESSAGE_LEN:
-                    ponder_pv.pop()
-                if ponder_pv[-1].endswith("."):
-                    ponder_pv.pop()
-                info["ponderpv"] = " ".join(ponder_pv)
-            except IndexError:
-                pass
-        return [f"{stat}: {info[stat]}" for stat in stats if stat in info]
+        pass
 
     def get_opponent_info(self, game):
         pass
 
     def name(self):
-        return self.engine.id["name"]
-
-    def report_game_result(self, game, board):
-        pass
-
-    def stop(self):
-        pass
+        return self.engine.name
 
     def quit(self):
         self.engine.quit()
 
+    def print_handler_stats(self, info, stats):
+        for stat in stats:
+            if stat in info:
+                print("    {}: {}".format(stat, info[stat]))
+
+    def get_handler_stats(self, info, stats):
+        stats_str = []
+        for stat in stats:
+            if stat in info:
+                stats_str.append("{}: {}".format(stat, info[stat]))
+
+        return stats_str
+
 
 class UCIEngine(EngineWrapper):
-    def __init__(self, commands, options, stderr, draw_or_resign):
-        super().__init__(commands, options, stderr, draw_or_resign)
-        self.engine = chess.engine.SimpleEngine.popen_uci(commands, stderr=stderr)
-        self.engine.configure(options)
+
+    def __init__(self, board, commands, options, silence_stderr=False):
+        commands = commands[0] if len(commands) == 1 else commands
+        self.go_commands = options.get("go_commands", {})
+
+        self.engine = engine.popen_uci(commands, stderr = subprocess.DEVNULL if silence_stderr else None)
+        
+
+        if options:
+            self.engine.configure(options)
+
+        self.engine.configure({
+            "UCI_Variant": type(board).uci_variant,
+            "UCI_Chess960": board.chess960
+        })
+        self.engine.position(board)
+
+        info_handler = chess.uci.Info()
+        self.engine.info_handlers.append(info_handler)
+
+
+    def first_search(self, board, movetime):
+        self.engine.position(board)
+        best_move, _ = self.engine.go(movetime=movetime)
+        return best_move
+
+    def search_with_ponder(self, board, wtime, btime, winc, binc, ponder=False):
+        self.engine.position(board)
+        best_move, ponder_move = self.engine.go(
+            wtime=wtime,
+            btime=btime,
+            winc=winc,
+            binc=binc,
+            ponder=ponder
+        )
+        return ( best_move , ponder_move )
+
+    def search(self, board, wtime, btime, winc, binc):
+        self.engine.position(board)
+        cmds = self.go_commands
+        best_move, _ = self.engine.go(
+            wtime=wtime,
+            btime=btime,
+            winc=winc,
+            binc=binc,
+            depth=cmds.get("depth"),
+            nodes=cmds.get("nodes"),
+            movetime=cmds.get("movetime")
+        )
+        return best_move
+
 
     def stop(self):
-        self.engine.protocol.send_line("stop")
+        self.engine.stop()
+
+
+    def print_stats(self):
+        self.print_handler_stats(self.engine.info_handlers[0].info, ["string", "depth", "nps", "nodes", "score"])
+
+
+    def get_stats(self):
+        return self.get_handler_stats(self.engine.info_handlers[0].info, ["depth", "nps", "nodes", "score"])
 
     def get_opponent_info(self, game):
         name = game.opponent.name
-        if name and "UCI_Opponent" in self.engine.protocol.config:
+        if name:
             rating = game.opponent.rating if game.opponent.rating is not None else "none"
             title = game.opponent.title if game.opponent.title else "none"
             player_type = "computer" if title == "BOT" else "human"
-            self.engine.configure({"UCI_Opponent": f"{title} {rating} {player_type} {name}"})
-
-    def report_game_result(self, game, board):
-        self.engine.protocol._position(board)
-
+            self.engine.setoption({"UCI_Opponent": "{} {} {} {}".format(title, rating, player_type, name)})
 
 class XBoardEngine(EngineWrapper):
-    def __init__(self, commands, options, stderr, draw_or_resign):
-        super().__init__(commands, options, stderr, draw_or_resign)
-        self.engine = chess.engine.SimpleEngine.popen_xboard(commands, stderr=stderr)
-        egt_paths = options.pop("egtpath", {}) or {}
-        features = self.engine.protocol.features
-        egt_types_from_engine = features["egt"].split(",") if "egt" in features else []
-        for egt_type in egt_types_from_engine:
-            options[f"egtpath {egt_type}"] = egt_paths[egt_type]
-        self.engine.configure(options)
 
-    def report_game_result(self, game, board):
-        # Send final moves, if any, to engine
-        self.engine.protocol._new(board, None, {})
+    def __init__(self, board, commands, options=None, silence_stderr=False):
+        commands = commands[0] if len(commands) == 1 else commands
+        self.engine = chess.xboard.popen_engine(commands, stderr = subprocess.DEVNULL if silence_stderr else None)
 
-        winner = game.state.get('winner')
-        termination = game.state.get('status')
+        self.engine.xboard()
 
-        if winner == 'white':
-            game_result = GameEnding.WHITE_WINS
-        elif winner == 'black':
-            game_result = GameEnding.BLACK_WINS
-        elif termination == Termination.DRAW:
-            game_result = GameEnding.DRAW
-        else:
-            game_result = GameEnding.INCOMPLETE
+        if board.chess960:
+            self.engine.send_variant("fischerandom")
+        elif type(board).uci_variant != "chess":
+            self.engine.send_variant(type(board).uci_variant)
 
-        if termination == Termination.MATE:
-            endgame_message = winner.title() + ' mates'
-        elif termination == Termination.TIMEOUT:
-            endgame_message = 'Time forfeiture'
-        elif termination == Termination.RESIGN:
-            resigner = 'black' if winner == 'white' else 'white'
-            endgame_message = resigner.title() + ' resigns'
-        elif termination == Termination.ABORT:
-            endgame_message = 'Game aborted'
-        elif termination == Termination.DRAW:
-            if board.is_fifty_moves():
-                endgame_message = '50-move rule'
-            elif board.is_repetition():
-                endgame_message = 'Threefold repetition'
+        if options:
+            self._handle_options(options)
+
+        if board.fen() != chess.STARTING_FEN:
+            self.engine.force()
+            if board.root().fen() != chess.STARTING_FEN:
+                self.engine.setboard(board.root())
+            for move in board.move_stack:
+                self.engine.usermove(move)
+        self.last_fen_seen = board.fen()
+
+        post_handler = chess.xboard.PostHandler()
+        self.engine.post_handlers.append(post_handler)
+
+    def _handle_options(self, options):
+        for option, value in options.items():
+            if option == "memory":
+                self.engine.memory(value)
+            elif option == "cores":
+                self.engine.cores(value)
+            elif option == "egtpath":
+                for egttype, egtpath in value.items():
+                    try:
+                        self.engine.egtpath(egttype, egtpath)
+                    except chess.EngineStateException:
+                        # If the user specifies more TBs than the engine supports, ignore the error.
+                        pass
             else:
-                endgame_message = 'Draw by agreement'
-        elif termination:
-            endgame_message = termination
+                try:
+                    self.engine.features.set_option(option, value)
+                except chess.EngineStateException:
+                    pass
+
+    def set_time_control(self, game):
+        self.minutes = game.clock_initial / 1000 // 60
+        self.seconds = game.clock_initial // 1000 % 60
+        self.inc = game.clock_increment // 1000
+        self.send_time()
+
+    def send_time(self):
+        self.engine.level(0, self.minutes, self.seconds, self.inc)
+
+    def send_last_move(self, board):
+        if board.fen() == self.last_fen_seen:
+            return
+        self.engine.force()
+        try:
+            self.engine.usermove(board.peek())
+        except IndexError:
+            self.engine.setboard(board)
+        self.last_fen_seen = board.fen()
+
+    def first_search(self, board, movetime):
+        self.send_last_move(board)
+        self.engine.st(movetime // 1000)
+        bestmove = self.engine.go()
+        self.send_time()
+
+        return bestmove
+
+    def search(self, board, wtime, btime, winc, binc):
+        self.send_last_move(board)
+
+        # XBoard engines expect time in units of 1/100 seconds.
+        if board.turn == chess.WHITE:
+            self.engine.time(wtime // 10)
+            self.engine.otim(btime // 10)
         else:
-            endgame_message = ''
+            self.engine.time(btime // 10)
+            self.engine.otim(wtime // 10)
+        return self.engine.go()
 
-        if endgame_message:
-            endgame_message = ' {' + endgame_message + '}'
+    def search_with_ponder(self, board, wtime, btime, winc, binc, ponder=False):
+        return self.search(board, wtime, btime, winc, binc), None
 
-        self.engine.protocol.send_line('result ' + game_result + endgame_message)
+    def print_stats(self):
+        self.print_handler_stats(self.engine.post_handlers[0].post, ["depth", "nodes", "score"])
 
-    def stop(self):
-        self.engine.protocol.send_line("?")
+    def get_stats(self):
+        return self.get_handler_stats(self.engine.post_handlers[0].post, ["depth", "nodes", "score"])
+
+
+    def name(self):
+        try:
+            return self.engine.features.get("myname")
+        except:
+            return None
 
     def get_opponent_info(self, game):
-        if game.opponent.name and self.engine.protocol.features.get("name", True):
-            title = game.opponent.title + " " if game.opponent.title else ""
-            self.engine.protocol.send_line(f"name {title}{game.opponent.name}")
+        title = game.opponent.title + " " if game.opponent.title else ""
+        if game.opponent.name:
+            self.engine.opponent_name(title + game.opponent.name)
         if game.me.rating is not None and game.opponent.rating is not None:
-            self.engine.protocol.send_line(f"rating {game.me.rating} {game.opponent.rating}")
+            self.engine.rating(game.me.rating, game.opponent.rating)
         if game.opponent.title == "BOT":
-            self.engine.protocol.send_line("computer")
-
-
-def getHomemadeEngine(name):
-    import strategies
-    return eval(f"strategies.{name}")
+            self.engine.computer()
