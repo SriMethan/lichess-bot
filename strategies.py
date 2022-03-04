93875990e2cd85abf9e6d9bd284c84463f5d711d
Methan Sritharan <methanroyal@gmail.com>
2022-03-04 05:32:13 +0000

    Minor Changes

--- strategies.py
+++ strategies.py
@@ -4,6 +4,7 @@ And some handy classes to extend
 """
 
 import chess
+from chess.engine import PlayResult
 import random
 from engine_wrapper import EngineWrapper
 
@@ -44,32 +45,28 @@ class MinimalEngine(EngineWrapper):
     however you can also change other methods like
     `notify`, `first_search`, `get_time_control`, etc.
     """
-    def __init__(self, *args, name=None):
-        super().__init__(*args)
+    def __init__(self, commands, options, stderr, draw_or_resign, name=None, **popen_args):
+        super().__init__(options, draw_or_resign)
 
         self.engine_name = self.__class__.__name__ if name is None else name
 
-        self.last_move_info = []
         self.engine = FillerEngine(self, name=self.name)
         self.engine.id = {
             "name": self.engine_name
         }
 
-    def search_with_ponder(self, board, wtime, btime, winc, binc, ponder):
-        timeleft = 0
-        if board.turn:
-            timeleft = wtime
-        else:
-            timeleft = btime
-        return self.search(board, timeleft, ponder)
+    def search(self, board, time_limit, ponder, draw_offered):
+        """
+        The method to be implemented in your homemade engine
 
-    def search(self, board, timeleft, ponder):
+        NOTE: This method must return an instance of "chess.engine.PlayResult"
+        """
         raise NotImplementedError("The search method is not implemented")
 
     def notify(self, method_name, *args, **kwargs):
         """
         The EngineWrapper class sometimes calls methods on "self.engine".
-        "self.engine" is a filler property that notifies <self> 
+        "self.engine" is a filler property that notifies <self>
         whenever an attribute is called.
 
         Nothing happens unless the main engine does something.
@@ -89,14 +86,14 @@ class ExampleEngine(MinimalEngine):
 
 class RandomMove(ExampleEngine):
     def search(self, board, *args):
-        return random.choice(list(board.legal_moves))
+        return PlayResult(random.choice(list(board.legal_moves)), None)
 
 
 class Alphabetical(ExampleEngine):
     def search(self, board, *args):
         moves = list(board.legal_moves)
         moves.sort(key=board.san)
-        return moves[0]
+        return PlayResult(moves[0], None)
 
 
 class FirstMove(ExampleEngine):
@@ -104,4 +101,4 @@ class FirstMove(ExampleEngine):
     def search(self, board, *args):
         moves = list(board.legal_moves)
         moves.sort(key=str)
-        return moves[0]
+        return PlayResult(moves[0], None)
