235d87f91577c3ff64d843d17bd5442daf422ef1
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2022-04-16 21:36:57 +0530

    Delete startbot.sh

--- startbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-chmod +x engines/Developed_SF
-chmod a+x engines/fsf_linux
-
-### ONLY UNCOMMENT ONE START POINT ###
-
-python heroku_challenges.py         # This start point will accept challenges according to the config.yml
-
-# python heroku_matchmaking.py        # This start point will challenge other bots and decline all challenges. CAUTION!!! Don't run this 24/7, you will get rate limited by lichess.
